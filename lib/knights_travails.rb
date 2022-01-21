51c0349c02426b16c8610488ea5b966248ce1cb0
gcerv93 <gcerv93@protonmail.com>
2022-01-21 15:04:12 -0600

    reword add_children method

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -5,9 +5,10 @@ class Node
   attr_reader :location
   attr_accessor :children
 
-  def initialize(location)
+  def initialize(location, parent = nil)
     @location = location
     @children = []
+    @parent = parent
   end
 
   def add_children(helper = find_children_helper, location = @location)
@@ -15,8 +16,12 @@ class Node
     helper.each do |nums|
       moves[0] = location[0] + nums[0]
       moves[1] = location[1] + nums[1]
-      @children << Node.new([moves[0], moves[1]]) unless moves[0].negative? || moves[1].negative?
+      create_child([moves[0], moves[1]]) unless moves[0].negative? || moves[1].negative?
     end
+  end
+
+  def create_child(coords)
+    @children << Node.new([coords[0], coords[1]], location)
   end
 
   def find_children_helper
@@ -31,19 +36,20 @@ class Graph
 
   def initialize(root)
     @root = root
+    @visited = []
   end
 end
 
 # class for the knight object
 class Game
-  attr_accessor :start_point, :end_point
+  attr_accessor :start_point, :end_point, :graph
 
   def initialize
     @start_point = nil
     @end_point = nil
   end
 
-  def knight_moves(start_point, end_point)
+  def knight_moves(start_point)
     @start_point = create_graph(start_point)
   end
 
@@ -51,6 +57,6 @@ class Game
     start_node = Node.new(start_node)
     start_node.add_children
     graph = Graph.new(start_node)
-    graph.root
+    @graph = graph.root
   end
 end
